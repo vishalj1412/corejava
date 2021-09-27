421a23291c14b520beceb09a174265c5692124ba
vishalj1412 <89152210+vishalj1412@users.noreply.github.com>
2021-09-27 10:15:41 +0530

    ADD: Check entered alphabet is Vowel or Consonant .

--- VowelConsonant.java
+++ VowelConsonant.java
@@ -10,7 +10,7 @@ public class VowelConsonant {
         if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
             System.out.println(ch + " is vowel");
         else
-            System.out.println(ch + " is consonant");
+           System.out.println(ch + " is consonant");
 
     }
 }
